f09221f5bf144527542ec59e02af334b09d64f7c
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-21 19:39:05 +0530

    clickOperation

--- src/test/java/tutorialsPoint/HandlingMouseActions.java
+++ src/test/java/tutorialsPoint/HandlingMouseActions.java
@@ -2,8 +2,11 @@ package tutorialsPoint;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -17,21 +20,25 @@ public class HandlingMouseActions {
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("");
+		driver.get("https://www.tutorialspoint.com/selenium/practice/buttons.php");
 		driver.manage().window().maximize();
 	}
 
 	@Test(priority = 1)
 	public void clickOperation() {
-
+		Actions act = new Actions(driver);
+		WebElement clickBtn = driver.findElement(By.xpath("//button[normalize-space()='Click Me']"));
+		act.click(clickBtn);
+		String text = driver.findElement(By.xpath("//div[@id='welcomeDiv']")).getText();
+		System.out.println(text);
 	}
 
-	@Test(priority = 2)
+//	@Test(priority = 2)
 	public void contextClickOperation() {
 
 	}
 
-	@Test(priority = 3)
+//	@Test(priority = 3)
 	public void DoubleclickOperation() {
 
 	}
